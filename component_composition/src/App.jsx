611f7d144da1d06a6fbd434ad4d18a80c851938e
lucypetry <martina.falchi11@gmail.com>
2025-03-04 12:47:45 +0100

    esecizio component compositon

---
+++ component_composition/src/App.jsx
@@ -0,0 +1,11 @@
+import Container from "./Container";
+
+function App() {
+  return (
+    <Container title="Click me">
+      <p>Contenuto visibile solo in determinate condizioni!</p>
+    </Container>
+  );
+}
+
+export default App;
